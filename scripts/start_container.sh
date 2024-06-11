09208152b763d51a96684286eda2f6c08858e834
Jayendrasathya <116148912+Jayendrasathya@users.noreply.github.com>
2024-06-11 13:24:39 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull jayendraklouder/cicd-flask-app:latest
+docker pull jayendraklouder/cicd-flask-app:latest
 
 # Run the Docker image as a container
-sudo docker run -d -p 5000:5000 jayendraklouder/cicd-flask-app:latest
+docker run -d -p 5000:5000 jayendraklouder/cicd-flask-app:latest
