188bbbc4ffde9f9f568753b41e6b3d38fa2e58a0
Jayendrasathya <116148912+Jayendrasathya@users.noreply.github.com>
2024-07-03 16:18:16 +0530

    Update app.py

--- cicd-simple-flask-app/app.py
+++ cicd-simple-flask-app/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route("/")
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return "GREEN environment!!"
+    return "BLUE environment!!"
 
 # main driver function
 if __name__ == "__main__":
